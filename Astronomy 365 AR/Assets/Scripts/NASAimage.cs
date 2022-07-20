74823e85eeaddc2099842fd6cba42e92ba62c72c
MadanYN <madanyn3@gmail.com>
2022-07-20 21:20:00 +0530

    TTS api config

--- Astronomy 365 AR/Assets/Scripts/NASAimage.cs
+++ Astronomy 365 AR/Assets/Scripts/NASAimage.cs
@@ -9,18 +9,24 @@ using LitJson;
 
 public class NASAimage : MonoBehaviour
 {
-    private const string URL = "https://api.nasa.gov/planetary/apod?";
-    private const string API_KEY = "api_key=tcGnfgsNUys7Wy2n64qVKGFCpWzsTVxnV0gbitb2";
+    private const string NASA_URL = "https://api.nasa.gov/planetary/apod?";
+    private const string NASA_API_KEY = "api_key=tcGnfgsNUys7Wy2n64qVKGFCpWzsTVxnV0gbitb2";
+    private const string TTS_URL = "http://api.voicerss.org/?";
+    private const string TTS_API_KEY = "key=27aed633ec454662a91aa73b34a3fe9d";
 
     private Text explanation;
     private Text title;
     private Text date;
     private RawImage image;
+    public AudioSource audioSource;
+
+    private bool play = false;
 
     private string webData;
     private JsonData json;
 
     private string url;
+    private string toSpeech;
 
     private void Start()
     {
@@ -28,7 +34,8 @@ public class NASAimage : MonoBehaviour
         date = GameObject.Find("date").GetComponent<Text>();
         explanation = GameObject.Find("ExplanationText").GetComponent<Text>();
         image = GameObject.Find("RawImage").GetComponent<RawImage>();
-        //GameObject.Find("Button").GetComponent<Button>().onClick.AddListener(getData);
+        //audioSource = AudioSource.Find("AudioSource");
+        GameObject.Find("AudioPlayButton").GetComponent<Button>().onClick.AddListener(getSpeech);
         getData();
     }
 
@@ -36,7 +43,7 @@ public class NASAimage : MonoBehaviour
 
     private IEnumerator OnResponse()
     {
-        using(UnityWebRequest request = UnityWebRequest.Get(URL + API_KEY))
+        using (UnityWebRequest request = UnityWebRequest.Get(NASA_URL + NASA_API_KEY))
         {
             yield return request.SendWebRequest();
             webData = request.downloadHandler.text;
@@ -44,29 +51,56 @@ public class NASAimage : MonoBehaviour
         json = JsonMapper.ToObject(webData);
 
         results();
-        
+
     }
-  
+
     void results()
     {
         //showing output
         title.text = json["title"].ToString();
         explanation.text = json["explanation"].ToString();
-        date.text = json["date"].ToString();
+        toSpeech = json["title"].ToString()+" . "+json["explanation"].ToString();
+
+        string temp = json["date"].ToString();
+        date.text = temp.Substring(8) + "-" + temp[5] + temp[6] + "-" + temp[0] + temp[1] + temp[2] + temp[3];
+        //date.text = temp;
 
         url = json["url"].ToString();
         StartCoroutine(renderImage());
     }
     private IEnumerator renderImage()
     {
-        /*using(UnityWebRequest request = UnityWebRequest.Get(url))
-        {
-            yield return request.SendWebRequest();
-            renderer.material.mainTexture = request.downloadHandler.texture;
-        }*/
-        WWW imageLoad = new WWW(url);
+        /*WWW imageLoad = new WWW(url);
         yield return imageLoad;
 
-        image.texture = imageLoad.texture;
+        image.texture = imageLoad.texture;*/
+
+        using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(url))
+        {
+            yield return request.SendWebRequest();
+            image.texture = DownloadHandlerTexture.GetContent(request);
+        }
     }
+    void getSpeech()
+    {
+        play = !play;
+        StartCoroutine(TextToSpeech());
+    } 
+
+    private IEnumerator TextToSpeech()
+    {
+        string details = "&hl=en-us&v=Linda&src=Image of the day:  "+toSpeech;
+        using (UnityWebRequest request = UnityWebRequestMultimedia.GetAudioClip(TTS_URL + TTS_API_KEY + details, AudioType.WAV))
+        {
+            yield return request.SendWebRequest();
+            audioSource.clip = DownloadHandlerAudioClip.GetContent(request);
+            if (play)
+            {
+                audioSource.Play();
+            }
+            else
+                audioSource.Stop();
+        }
+    }
+
 }
